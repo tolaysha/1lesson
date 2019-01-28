054801efe1608e5f67472defcfc028fe5f2c1d6c
Tolay <tolaykuzay@gmail.com>
2019-01-29 01:23:54 +0300

    функции начало

--- js.js
+++ js.js
@@ -1,15 +1,29 @@
-//var money = +prompt('Ваш бюджет на месяц'),
-  //  time = prompt('Введите дату в формате YYYY-MM-DD');
-   
+var money ,time; 
+
+function start(){
+money = +prompt('Ваш бюджет на месяц'),
+time = prompt('Введите дату в формате YYYY-MM-DD');
+
+while (Number.isNaN(money) || money == ''|| money == null){
+    money = +prompt('Ваш бюджет на месяц');
+}
+}
+
+start();
 var appData = {
-    bujet : 1,
-    data : 1,
-    savings : false,
+    bujet : money,
+    data : time,
+    savings : true,
     expenses : {},
     optionalExpenses : {},
     income: []
     };
 
+    
+
+
+
+function ChooseExp(){
     for (i = 0; i < 2; i++){
         var a = prompt('Cтатья расходов', ''),
             b = +prompt('Сумма', '');
@@ -23,36 +37,16 @@ var appData = {
         }
     
     }
-   // appData.MoneyPerDay= appData.bujet / 30;
-    //alert('Your daily bujet = ' + appData.MoneyPerDay)
 
+}
+ChooseExp();
+    appData.MoneyPerDay= (appData.bujet / 30).toFixed();
+    alert('Your daily bujet = ' + appData.MoneyPerDay);
 
-
-
-
-
-
-
-
-/*    
-var num = 1;
-
-num++;
-console.log(num);
-x = ++nam;
-console.log(x);
-console.log(typeof(false));
-let y = 1; 
-let x = y = 2; 
-alert(x);
-
-var xx = 0 || "" || 2 || undefined || true || falsе;
-console.log(xx);
-xx = 0 || "" || 2 || undefined || true ;
-console.log(xx);
-xx = 0 || "" || 2 || undefined ;
-console.log(xx);
-xx = 0 || "" || 2 ;
-console.log(xx);
-var xx = 'A' > 'B' ;
-console.log(xx);*/
+function CheckSavings(){
+    if (appData.savings == true){
+        appData.MonthIncome= prompt('Сумма накоплений')/100 * prompt('Процент')/12;
+        alert('Месячный доход = ' + appData.MonthIncome);
+    }
+}
+CheckSavings();
